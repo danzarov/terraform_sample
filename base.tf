ca9786370293ebdb1deb5187a5204501c80ad002
danzarov <danzarov@gmail.com>
2019-11-28 22:21:43 -0300

    changes to use implicit reference

--- base.tf
+++ base.tf
@@ -16,17 +16,13 @@ resource "aws_s3_bucket" "b" {
 }
 
 resource "aws_s3_bucket_object" "test1" {
-  bucket  = "dannybucket99sss9"
+  bucket  = aws_s3_bucket.b.id
   key     = "test1.txt"
   content = timestamp()
-
-  depends_on = [aws_s3_bucket.b]
 }
 
 resource "aws_s3_bucket_object" "test2" {
-  bucket  = "dannybucket99sss9"
+  bucket  = aws_s3_bucket.b.id
   key     = "test2.txt"
   content = timestamp()
-
-  depends_on = [aws_s3_bucket.b]
 }
