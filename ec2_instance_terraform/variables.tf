e6e569b738c8521815f7bdcdf4ee134bf217940d
danzarov <danzarov@gmail.com>
2019-12-08 21:51:04 -0300

    cleanup

--- ec2_instance_terraform/variables.tf
+++ ec2_instance_terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS ACCESS KEY"
-}
-
-variable "aws_secret_key" {
-  description = "AWS SECRET KEY"
-}
-
-variable "aws_region" {
-  description = "AWS REGION"
-}
-
 variable "aws_instance_type" {
   description = "AWS INSTANCE TYPE"
 }
